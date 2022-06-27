eda256be308f0283b3ad1a950b5f8f613cbdbc8d
GuruYosh <62548319+GuruYosh@users.noreply.github.com>
2022-06-27 22:32:25 +0200

    Update duckdns-updater.sh

--- duckdns-updater.sh
+++ duckdns-updater.sh
@@ -4,14 +4,18 @@ domain="DOMINIO"
 token="T-O-K-E-N"
 logfile="duckdns.log"
 
+## Lista de servidores para preguntar IP WAN
 server_ip=("ifconfig.me" "ifconfig.co" "icanhazip.com" "http://ipecho.net/plain")
 rand_server=$(($RANDOM % ${#server_ip[@]}))
 
+## Elección de servidor DNS al que preguntar
 dnsserver=$(echo "ns$(( 1 + $RANDOM % 6)).duckdns.org")
 ip_dns="$(dig @"$dnsserver" "$domain".duckdns.org +short)"
 
+## IP WAN
 ip="$(curl -s ${server_ip[$rand_server]})"
 
+## Resultado de la operación de actualización de IP en duckdns
 result="NO"
 
 if [ "$ip" != "$ip_dns" ];
