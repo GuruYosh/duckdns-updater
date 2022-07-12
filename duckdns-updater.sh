f9d3fa741ca51b18581ee20f55152661b8f8f5aa
GuruYosh <62548319+GuruYosh@users.noreply.github.com>
2022-07-12 20:16:25 +0200

    Update duckdns-updater.sh

--- duckdns-updater.sh
+++ duckdns-updater.sh
@@ -13,14 +13,19 @@ timewait=10
 
 error=0
 
+# Función para loguear información en el fichero logfile proporcionado
+
 function loguear {
 	echo $(date +"%F %T") $1 >> $logfile
 }
 
+# Función para validar IP obtenida
+
 function isValidIpAddr() {
-    # https://stackoverflow.com/questions/13777387/check-for-ip-validity/
+    # return code only version
+	# https://stackoverflow.com/questions/13777387/check-for-ip-validity/
     # Autor: zpangwin & shannonman & Mitch Frazier
-    
+
     local ipaddr="$1";
     [[ ! $ipaddr =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] && return 1;
     for quad in $(echo "${ipaddr//./ }"); do
@@ -29,26 +34,35 @@ function isValidIpAddr() {
     done
 }
 
+# Comandos necesarios para poder ejecutar el script
 
 if ! [ $(which dig) ] || ! [ $(which curl) ]; then
 	loguear "Los comandos dig y curl deben estar instalados en el sistema"
 	exit 1
 fi
 
+# Servidores para obtener la IP (WAN)
+
 server_ip=("ifconfig.me" "ifconfig.co" "icanhazip.com" "ipecho.net/plain" "ipinfo.io/ip")
 rand_server=$(($RANDOM % ${#server_ip[@]}))
 
-dnsserver=$(echo "ns$(( 1 + $RANDOM % 6)).duckdns.org")
-
-ip_dns="$(dig @"$dnsserver" "$domain".duckdns.org +short)"
-
-isValidIpAddr $ip_dns
-
-error=$(( $error + $? ))
+# Obtenemos IP de la WAN
 
 ip="$(curl -s ${server_ip[$rand_server]})"
 
 isValidIpAddr $ip
+
+error=$(( $error + $? ))
+
+# DNS de duckdns
+
+dnsserver=$(echo "ns$(( 1 + $RANDOM % 6)).duckdns.org")
+
+# Obtenemos IP del DNS
+
+ip_dns="$(dig @"$dnsserver" "$domain".duckdns.org +short)"
+
+isValidIpAddr $ip_dns
 
 error=$(( $error + $? ))
 
@@ -62,11 +76,15 @@ result="NO"
 
 if [ "$ip" != "$ip_dns" ];
 then
+	# Actualizamos IP en servidor DNS de duckdns
+
 	result="$(curl -s "https://www.duckdns.org/update?domains=$domain&token=$token&ip=")"
 
 	loguear "${server_ip[$rand_server]} $dnsserver $ip_dns $ip $result"
 
 	sleep $timewait
+
+	# Obtenemos nuevamente la IP del DNS para ver si ha cambiado correctamente
 
 	ip_nueva="$(dig @"$dnsserver" "$domain".duckdns.org +short)"
 
